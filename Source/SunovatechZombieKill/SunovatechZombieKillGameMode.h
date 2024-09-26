213d4037d456e60d1a6a160ce0dd19793951b25f
BellInequality <ravimohan1991@gmail.com>
2024-09-26 21:04:17 +0530

    More documentation

--- Source/SunovatechZombieKill/SunovatechZombieKillGameMode.h
+++ Source/SunovatechZombieKill/SunovatechZombieKillGameMode.h
@@ -1,5 +1,11 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
+/**
+ * @file SunovatechZombieKillGameMode.h
+ * @author Ravi Mohan
+ * @brief This file contains ASunovatechZombieKillGameMode include. The file was generated from UE by Epic Games and modified by author.
+ * @version 1.0
+ * @date September 17, 2024
+ * @copyright Copyright Epic Games, Inc. All Rights Reserved. Ravi Mohan accepted UE(5) license.
+ */
 #pragma once
 
 #include "CoreMinimal.h"
@@ -12,6 +18,12 @@ class ASunovatechZombieKillGameMode : public AGameModeBase
 	GENERATED_BODY()
 
 public:
+
+	/**
+	 * @brief A constructor for setting the PlayerController class
+	 *
+	 * @note We also set simillar style in blueprints. Ponder over.
+	 */
 	ASunovatechZombieKillGameMode();
 };
 
